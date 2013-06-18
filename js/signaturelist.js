5dca09a3325bd6ce48adc579187c17913569064d
Jeremy Malcolm <Jeremy@Malcolm.id.au>
2013-06-18 19:05:55 +0800

    Split signature list into organisations and individuals

--- js/signaturelist.js
+++ js/signaturelist.js
@@ -111,3 +111,119 @@ jQuery( document ).ready( function( $ ) {
 	}
 
 });
+
+
+
+jQuery( document ).ready( function( $ ) {
+    'use strict';
+
+    // next pagination button is clicked
+    $( '.dk-speakup-signaturelist-org-next' ).click( function( e ) {
+        e.preventDefault();
+        get_signaturelist_org( $( this ) );
+    });
+
+    // prev pagination button is clicked
+    $( '.dk-speakup-signaturelist-org-prev' ).click( function( e ) {
+        e.preventDefault();
+        get_signaturelist_org( $( this ) );
+    });
+
+    // pagination: query new signatures and display results
+    function get_signaturelist_org( button, link ) {
+        // change button appearance to disabled while ajax request is processing
+        $( this ).addClass( 'dk-speakup-signaturelist-org-disabled' );
+        
+        var link   = button.attr( 'rel' ).split( ',' ),
+            id     = link[0],
+            start  = link[1],
+            limit  = link[2],
+            total  = link[3],
+            status = link[4],
+            ajax   = {
+                action: 'dk_speakup_paginate_signaturelist_org',
+                id:         id,
+                start:      start,
+                limit:      limit,
+                dateformat: dk_speakup_signaturelist_js.dateformat
+            };
+
+        if ( status === '1' ) {
+            // submit data and handle ajax response
+            $.post( dk_speakup_signaturelist_js.ajaxurl, ajax,
+                function( response ) {
+                    var next_link = get_next_link_org( id, start, limit, total );
+                    var prev_link = get_prev_link_org( id, start, limit, total );
+
+                    toggle_button_display_org( id, next_link, prev_link );
+
+                    $( '.dk-speakup-signaturelist-org-' + id + ' tr:not(:last-child)' ).remove();
+                    $( '.dk-speakup-signaturelist-org-' + id ).prepend( response );
+                    $( '.dk-speakup-signaturelist-org-' + id + ' .dk-speakup-signaturelist-org-next' ).attr( 'rel', next_link );
+                    $( '.dk-speakup-signaturelist-org-' + id + ' .dk-speakup-signaturelist-org-prev' ).attr( 'rel', prev_link );
+                }
+            );
+        }
+    }
+
+    // format new link for next pagination button
+    function get_next_link_org( id, start, limit, total ) {
+        var start = parseInt( start ),
+            limit = parseInt( limit ),
+            total = parseInt( total ),
+            new_start = '',
+            status    = '',
+            link      = '';
+
+        if ( start + limit  < total ) {
+            new_start = start + limit;
+            status = '1';
+        }
+        else {
+            new_start = total;
+            status = '0';
+        }
+
+        link = id + ',' + new_start + ',' + limit + ',' + total + ',' + status;
+        return link;
+    }
+
+    // format new link for prev pagination button
+    function get_prev_link_org( id, start, limit, total ) {
+        var start = parseInt( start ),
+            limit = parseInt( limit ),
+            total = parseInt( total ),
+            new_start = '',
+            status    = '',
+            link      = '';
+
+        if ( start - limit >= 0 ) {
+            new_start = start - limit;
+            status = '1';
+        }
+        else {
+            new_start = total;
+            status = '0';
+        }
+
+        link = id + ',' + new_start + ',' + limit + ',' + total + ',' + status;
+        return link;
+    }
+
+    function toggle_button_display_org( id, next_link, prev_link ) {
+        if ( next_link.split( ',' )[4] === '0' ) {
+            $( '.dk-speakup-signaturelist-org-' + id + ' .dk-speakup-signaturelist-org-next' ).addClass( 'dk-speakup-signaturelist-org-disabled' );
+        }
+        else {
+            $( '.dk-speakup-signaturelist-org-' + id + ' .dk-speakup-signaturelist-org-next' ).removeClass( 'dk-speakup-signaturelist-org-disabled' );
+        }
+
+        if ( prev_link.split( ',' )[4] === '0' ) {
+            $( '.dk-speakup-signaturelist-org-' + id + ' .dk-speakup-signaturelist-org-prev' ).addClass( 'dk-speakup-signaturelist-org-disabled' );
+        }
+        else {
+            $( '.dk-speakup-signaturelist-org-' + id + ' .dk-speakup-signaturelist-org-prev' ).removeClass( 'dk-speakup-signaturelist-org-disabled' );
+        }
+    }
+
+});
